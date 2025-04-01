3a7e3ceab1b05f79d54efa319b2feebe1781632a
Deekshith SH <shdeekshith@gmail.com>
2025-04-01 23:55:16 +0530

    Testing

---
+++ app.py
@@ -0,0 +1,12 @@
+# Test Keyword
+
+from os import environ
+from aiohttp import web
+async def stats(request):
+    return web.json_response({"Hi": "Hello"})
+
+app = web.Application()
+app.add_routes([web.get('/', stats)])
+
+if __name__ == "__main__":
+    web.run_app(app, host='0.0.0.0', port=environ.get("PORT"))
