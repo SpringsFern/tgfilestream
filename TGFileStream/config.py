d7d21000a3456d99cf35e245007c4dc000c9771f
Deekshith SH <shdeekshith@gmail.com>
2025-04-02 17:00:28 +0530

    Added argument parsing to accept command-line options

--- TGFileStream/config.py
+++ TGFileStream/config.py
@@ -15,14 +15,26 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import sys
 import os
+import argparse
 
 from yarl import URL
 
 from dotenv import load_dotenv
-load_dotenv()
+
+parser = argparse.ArgumentParser(description="A Telegram bot that can stream Telegram files to users over HTTP.")
+parser.add_argument("--api-id", type=int, help="Your Telegram API ID")
+parser.add_argument("--api-hash", type=str, help="Your Telegram API Hash")
+parser.add_argument("--bot-token", type=str, help="Your Telegram Bot Token")
+parser.add_argument("--env", type=str, default=".env", help="Path to the environment file (default: .env)")
+parser.add_argument("--host", type=str, help="Override the host address")
+parser.add_argument("--port", type=int, help="Override the port number")
+parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+args = parser.parse_args()
+
+load_dotenv(args.env)
 
 try:
-    port = int(os.environ.get("PORT", "8080"))
+    port = args.port or int(os.environ.get("PORT", "8080"))
 except ValueError:
     port = -1
 if not 1 <= port <= 65535:
@@ -30,21 +42,21 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["TG_API_ID"])
-    api_hash = os.environ["TG_API_HASH"]
+    api_id = args.api_id or int(os.environ["TG_API_ID"])
+    api_hash = args.api_hash or os.environ["TG_API_HASH"]
 except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
     sys.exit(1)
 
 trust_headers = bool(os.environ.get("TRUST_FORWARD_HEADERS"))
-host = os.environ.get("HOST", "0.0.0.0")
+host = args.host or os.environ.get("HOST", "0.0.0.0")
 public_url = URL(os.environ.get("PUBLIC_URL", f"http://{host}:{port}"))
 
 session_name = os.environ.get("TG_SESSION_NAME", "TGFileStream")
 
 log_config = os.environ.get("LOG_CONFIG")
-debug = bool(os.environ.get("DEBUG"))
+debug = args.debug or bool(os.environ.get("DEBUG"))
 
 try:
     # The per-user ongoing request limit
@@ -64,4 +76,4 @@ except ValueError:
 start_message = os.environ.get("TG_START_MESG", "Send an image or file to get a link to download it")
 group_chat_message = os.environ.get("TG_G_C_MESG", "Sorry. But, I only work in private.")
 
-tg_bot_token = os.environ.get("TG_BOT_FATHER_TOKEN", None)
+tg_bot_token = args.bot_token or os.environ.get("TG_BOT_FATHER_TOKEN", None)
